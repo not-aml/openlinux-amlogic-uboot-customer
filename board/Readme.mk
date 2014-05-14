6f50fd863e73e204d1590a2955dd87dea9eb8dcb
dhead666 <myfoolishgames@gmail.com>
2014-05-14 21:15:14 +0300

    import 20140306-09887e87b4

---
+++ board/Readme.mk
@@ -0,0 +1,193 @@
+help_customer:
+
+	@echo
+	@echo customer defined board begin
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "RAMOS_AML8726_MX_REF_A"
+	@echo config command: \"make m6_ramos_v1_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "AINOL_AML8726_V1_REF_A"
+	@echo config command: \"make m6_ainol_v1_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "AINOL_AML8726_848_REF_A"
+	@echo config command: \"make m6_ainol_848_v1_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "AINOL_AML8726_E3_REF_A"
+	@echo config command: \"make m6_ainol_e3_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "AINOL_AML8726_E4_REF_A"
+	@echo config command: \"make m6_ainol_e4_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "EMDOOR_M6_1024_600_REF_A"
+	@echo config command: \"make m6_emdoor_1024_600_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "EMDOOR_M6_1024_768_REF_A"
+	@echo config command: \"make m6_emdoor_1024_768_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "NEWSMY_AML8726_MX_REF_A"
+	@echo config command: \"make m6_newsmy_v1_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "WINACCORD_AML8726_MX_REF_A"
+	@echo config command: \"make m6_winaccord_v1_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "m6_yifang_m7000nbd"
+	@echo config command: \"make m6_yifang_m7000nbd_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "MBX_AML8726_MX_REF_A" with TEE
+	@echo config command: \"make m6_mbx_v1_tee_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB Ref Board"
+	@echo config command: \"make m6_chinach_v1_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB Ref Board" with TEE
+	@echo config command: \"make m6_chinach_v1_tee_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB MXL Ref Board"
+	@echo config command: \"make m6_g33new_512M_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB MXL Ref Board"
+	@echo config command: \"make m6_g33new_1GB_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB MXS HENGCHEN MID807K"
+	@echo config command: \"make m6_g33_MID807K_config\"
+		
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB MXL 1212 Ref Board"
+	@echo config command: \"make m6_g33_1212_config\"
+
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB ASD Board PI3100"
+	@echo config command: \"make m6_asd_pi3100_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB ASD Board PI3900"
+	@echo config command: \"make m6_asd_pi3900_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "DONGLE_AML8726_MX_REF_A"
+	@echo config command: \"make m6_dongle_g40ref_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "MBX_AML8726_MX_REF_A"
+	@echo config command: \"make mx_dongle_g02_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "DUOKAN_MBX_AML8726_MX_REF_A"
+	@echo config command: \"make m6_duokan_mbx_g19ref_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "DVB_g17_M6_AML8726_REF_A"
+	@echo config command: \"make m6_dvb_g17_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "M6TV_H02_V1"
+	@echo config command: \"make m6tv_h02_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "M6TV_H03_V1"
+	@echo config command: \"make m6tv_h03_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "M6TV_H04_V1"
+	@echo config command: \"make m6tv_h04_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "M6TV_H05_V1"
+	@echo config command: \"make m6tv_h05_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "M6TV_H06_V1"
+	@echo config command: \"make m6tv_h06_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "GADMEI_M6_G11T80_REF_A"
+	@echo config command: \"make m6_gadmei_g11t80_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k01"
+	@echo config command: \"make m8_k01_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k03_M101_v1"
+	@echo config command: \"make m8_k03_M101_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k03_M102_v1"
+	@echo config command: \"make m8_k03_M102_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k03_M901_v1"
+	@echo config command: \"make m8_k03_M901_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k03_M902_v1"
+	@echo config command: \"make m8_k03_M902_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "M8_K04_M3X13_V1"
+	@echo config command: \"make m8_k04_m3x13_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k05_hp"
+	@echo config command: \"make m8_k05_hp_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k06_NabiJR_1G"
+	@echo config command: \"make m8_k06_NabiJR_1G_config\"
+	
+	@echo =======================================================================
+	@echo The mark in board is "m8_k06_NabiJR_512M"
+	@echo config command: \"make m8_k06_NabiJR_512M_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k06_Nabi2C_v1"
+	@echo config command: \"make m8_k06_Nabi2C_v1_config\"
+
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k08_v1"
+	@echo config command: \"make m8_k08_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k08_73q7_v1"
+	@echo config command: \"make m8_k08_73q7_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k09_v1"
+	@echo config command: \"make m8_k09_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "m8_k12_MA975M8_v1"
+	@echo config command: \"make m8_k12_MA975M8_v1_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "M8_PXP"
+	@echo config command: \"make m8_pxp_config\"
+
+	@echo =======================================================================
+	@echo The mark in board is "M8_ZEBU"
+	@echo config command: \"make m8_ZeBu_config\"
+
+	@echo ----------------------------------------------------------------------
+	@echo The mark in board is "Amlogic JB MXS Ref Board"
+	@echo config command: \"make m6_g24_k7_2GB_config\"
+
+	@echo customer defined board end
+	@echo
+
